4f40b185a5032a4ac62362fb9828ebaaa916c831
Akansh-Agrawal <agrawalakansh01@gmail.com>
2022-03-09 00:26:49 +0530

    demo

--- my-arpo/src/App.js
+++ my-arpo/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import StudentDashboard from './pages/StudentDashboard';
-
+import Instructor_dash from './pages/InstructorHomepage';
 function App() {
   return (
     <div className="App ">
-      {/* <LoginPage /> */}
-      <StudentDashboard />
+      <LoginPage />
+      {/* <StudentDashboard /> */}
+      {/* <InstructorHomepage /> */}
     </div>
   );
 }
