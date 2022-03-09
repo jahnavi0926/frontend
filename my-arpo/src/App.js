6e6e6de7f3d9a6d7dd6b03f5c261356299ccda9a
your_github_username <pranavsingh560@gmail.com>
2022-03-10 03:22:39 +0530

    added subjectdescription component and also updated genquery and privatequery pages

--- my-arpo/src/App.js
+++ my-arpo/src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import LoginPage from './pages/LoginPage';
 import StudentDashboard from './pages/StudentDashboard';
 import InstructorHomepage from './pages/InstructorHomepage';
-// import { GenQueryAddition } from './pages/GenQueryAddition';
-// import { PrivateQueryAddition} from './pages/PrivateQueryAddition'
+import GenQueryAddition from './pages/GenQueryAddition';
+import PrivateQueryAddition from './pages/PrivateQueryAddition'
 function App() {
   return (
     <div className="App ">
       {/* <LoginPage /> */}
       {/* <StudentDashboard /> */}
-       <InstructorHomepage />
+       {/* <InstructorHomepage /> */}
        {/* <GenQueryAddition />  */}
-      {/* <PrivateQueryAddition /> */}
+      <PrivateQueryAddition />
     </div>
   );
 }
--- my-arpo/src/pages/GenQueryAddition.js
+++ my-arpo/src/pages/GenQueryAddition.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import SubjectDescription from '../components/SubjectDescription'
 
-const GenQueryAddition = () => {
-  return (
-    <>
-      <div>GenQueryAddition</div>
-      <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-        <label className="form-check-label" htmlfor="flexCheckDefault">
-          Anonymous checkbox
-        </label>
-      </div>
-    </>
+function GenQueryAddition() {
+  return (<>
+    <div>General Query Addition</div>
+
+    {/* Anonymous toggle */}
+    <div className="form-check form-switch" textAlign='right'>
+      <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+      <label className="form-check-label" for="flexSwitchCheckDefault">Anonymous</label>
+    </div>
+    < SubjectDescription />
+
+    {/* add button */}
+    <button type="button" class="btn btn-primary">Add</button>
+  </>
   )
 }
 
